2341554b7fc464260790b4c16bbc5fddd8f9d4b5
laihaibo <18768101327@163.com>
2017-07-21 20:30:51 +0800

    更新pages的算法，可以下载所欲偶的相册和图片

--- index.js
+++ index.js
@@ -29,6 +29,23 @@ const getHtml = url => {
   })
 }
 
+// 页码
+const getPages = (doms) => {
+  return [...new Set(doms.map(function (i, el) {
+    let num = el.attribs.href.split('/').pop().split('.').shift()
+    return num === '' ? 1 : parseInt(num, 10)
+    // return el.attribs
+  }).get())].reduce((prev, cur) => Math.max(prev, cur), 0)
+}
+
+const getImgPages = (doms) => {
+  return doms.map(function (i, el) {
+    let num = el.attribs.href.split('/');
+    return num.length === 3 ? 1 : parseInt(num.pop().split('.').shift(), 10);
+    // return num;
+  }).get().reduce((prev, cur) => Math.max(prev, cur), 0)
+}
+
 //获取指定页码范围内的所有图册代码
 const getAlbums = (startUrl) => {
   return new Promise((resolve, reject) => {
@@ -36,20 +53,16 @@ const getAlbums = (startUrl) => {
     let getQuery = async startUrl => {
       try {
         let $ = await getHtml(startUrl);
-        let pages = $('#listdiv .pagesYY a').length;
+        let pages = getPages($('#listdiv .pagesYY a'));
         // let pages = 20;
         for (let i = 1; i <= pages; i++) { // <=在只有一页时可以用
-          let pageUrl = `${startUrl + i}.html`
+          let pageUrl = i === 1 ? startUrl : `${startUrl + i}.html`
           let $ = await getHtml(pageUrl);
 
-          let mmm = $('#listdiv .pagesYY a').map(function (i, el) {
-            return parseInt($(this).text(), 0);
-          }).get().filter(x => x > 0);
-          pages = mmm.length < 2 ? pages : mmm.reduce((prev, cur) => Math.max(prev, cur));
-          // console.log(pages);
+          // 更新pages
+          pages = getPages($('#listdiv .pagesYY a'));
 
           $('.galleryli_title a').each(function () {
-            // console.log(this);
             albums.push({
               title: $(this).text(),
               url: `https://www.nvshens.com${$(this).attr("href")}`,
@@ -71,15 +84,20 @@ const getAlbums = (startUrl) => {
 //获取指定页码范围内的所有图册代码
 const getImgList = (startUrl) => {
   return new Promise((resolve, reject) => {
-    console.log(`开始爬取 ${startUrl} ...`);
+    // console.log(`开始爬取 ${startUrl} ...`);
     let albums = [];
     let getQuery = async startUrl => {
       try {
         let $ = await getHtml(startUrl);
-        let pages = $('#pages a').length;
+        // let pages = $('#pages a').length;
+        let pages = getImgPages($('#pages a'));
         for (let i = 1; i <= pages; i++) {
-          let pageUrl = `${startUrl + i}.html`
+          let pageUrl = i === 1 ? startUrl : `${startUrl + i}.html`;
           let $ = await getHtml(pageUrl);
+
+          // 更新pageUrl
+          pages = getImgPages($('#pages a'));
+
           $('#hgallery img').each(function () {
             let url = $(this).attr('src');
             let fileName = url.split('/').pop();
@@ -92,7 +110,7 @@ const getImgList = (startUrl) => {
           })
         }
         resolve(albums);
-        console.log(`爬取 ${startUrl} 结束...`)
+        // console.log(`爬取 ${startUrl} 结束...`)
       } catch (error) {
         console.log(error);
       }
